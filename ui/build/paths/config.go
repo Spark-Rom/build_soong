8d1ddb6de5646ba7961c5020277d2aa420e8e305
TextsFromYourEx <skgaming7869@gmail.com>
2020-10-19 09:23:36 +0200

    soong: Whitelist m4
    Signed-off-by: TextsFromYourEx <skgaming7689@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -91,6 +91,7 @@ var Configuration = map[string]PathConfig{
 	"java":    Allowed,
 	"javap":   Allowed,
 	"lsof":    Allowed,
+	"m4":      Allowed,
 	"nproc":   Allowed,
 	"openssl": Allowed,
 	"patch":   Allowed,
