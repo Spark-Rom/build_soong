ayanm09/platform_build_soong

soong: Hack to preserve Octavi product variables
Change-Id: Ice4093b86baed6974bab630f0e80f6b13a5e28f3

android/variable.go
@@ -601,6 +601,11 @@ func createVariableProperties(moduleTypeProps []interface{}, productVariables in
 func createVariablePropertiesType(moduleTypeProps []interface{}, productVariables interface{}) reflect.Type {
 	typ, _ := proptools.FilterPropertyStruct(reflect.TypeOf(productVariables),
 		func(field reflect.StructField, prefix string) (bool, reflect.StructField) {
+			if strings.HasPrefix(prefix, "Product_variables.Octavi") {
+				// Convert Product_variables.Octavi.Foo to Octavi.Foo
+				_, prefix = splitPrefix(prefix)
+			}
+
 			// Filter function, returns true if the field should be in the resulting struct
 			if prefix == "" {
 				// Keep the top level Product_variables field

Spark-Rom/build_soong

android/variable.go
@@ -601,6 +601,11 @@ func createVariableProperties(moduleTypeProps []interface{}, productVariables in
 func createVariablePropertiesType(moduleTypeProps []interface{}, productVariables interface{}) reflect.Type {
 	typ, _ := proptools.FilterPropertyStruct(reflect.TypeOf(productVariables),
 		func(field reflect.StructField, prefix string) (bool, reflect.StructField) {
+			if strings.HasPrefix(prefix, "Product_variables.Spark") {
+				// Convert Product_variables.Spark.Foo to Spark.Foo
+				_, prefix = splitPrefix(prefix)
+			}
+
 			// Filter function, returns true if the field should be in the resulting struct
 			if prefix == "" {
 				// Keep the top level Product_variables field