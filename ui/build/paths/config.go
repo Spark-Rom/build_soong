f2eee7fbcb3b7dc26d0d8abea78c1ec09e5b3c9b
Vitorgl2003 <vitorgl.2003@gmail.com>
2022-01-26 02:05:48 +0530

    soong: Allow aarch64-linux-gnu-as
    Change-Id: Ic32dd1dc06987dac595bceb69b70628700f3602a

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -97,6 +97,7 @@ var Configuration = map[string]PathConfig{
 	"zip":     Allowed,
 	"arm-linux-androidkernel-as": Allowed,
 	"arm-linux-androidkernel-ld": Allowed,
+	"aarch64-linux-gnu-as": Allowed,
 
 	// Host toolchain is removed. In-tree toolchain should be used instead.
 	// GCC also can't find cc1 with this implementation.
